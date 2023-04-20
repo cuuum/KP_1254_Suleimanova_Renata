cf91b4ec7089f01f4f4a24ca89036a442eb42d65
cuuum <renata1s@ya.ru>
2023-04-20 13:43:47 +0300

    Сделала новую ветку для второго задания

---
+++ kp2/kp2/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace kp2
+{
+    public partial class Form1 : Form
+    {
+        List<string> bufferAns;
+        TestResult autoTestRes;
+
+        List<TestResult> results = new List<TestResult>(100);
+
+        // разделитель для ответов на 2 вопрос
+        char sep = '$';
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // задаем авто - ответы
+            List<string> autoAns = new List<string> { radioButton1.Text, checkBox1.Text + sep + checkBox2.Text, comboBox2.Items[0].ToString(), "10", "Здесь могла бы быть ваша реклама" };
+            autoTestRes = new TestResult("Василий", "Смагин", "0", "1254", autoAns);
+
+            bufferAns = autoAns;
+        }
+
+        // вытягиваем ответы из формы и сохраняем в класс
+        private TestResult Get_Ans()
+        {
+
+            string name = textBox1.Text;
+            string sername = textBox2.Text;
+            string age = numericUpDown1.Value.ToString();
+            string group = comboBox1.Text;
+
+            bufferAns = Enumerable.Repeat("", bufferAns.Count).ToList();
+
+            // 1
+            foreach (var i in new List<RadioButton> { radioButton1, radioButton2, radioButton3 })
+            {
+                if (i.Checked)
+                {
+                    bufferAns[0] = i.Text;
+                    break;
+                }
+            }
+
+            // 2
+            foreach (var i in new List<CheckBox> { checkBox1, checkBox2, checkBox3, checkBox4 })
+            {
+
+
+                //Console.WriteLine(i.Text);
+                if (i.Checked)
+                {
+                    bufferAns[1] += i.Text + sep;
+                }
+            }
+
+            // 3
+            bufferAns[2] = comboBox2.Text;
+
+            // 4
+            bufferAns[3] = numericUpDown2.Value.ToString();
+
+            // 5
+            bufferAns[4] = richTextBox1.Text;
+
+            return new TestResult(name, sername, age, group, bufferAns);
+
+        }
+
+        // устанавливаем ответы в форму
+        private void Set_Ans(TestResult ans)
+        {
+
+            textBox1.Text = ans.Name;
+            textBox2.Text = ans.Sername;
+
+            if (Int32.TryParse(ans.Age, out int answ))
+                numericUpDown1.Value = answ;
+
+            comboBox1.Text = ans.Group;
+
+            bufferAns = Enumerable.Repeat("", bufferAns.Count).ToList();
+
+            // 1
+            foreach (var i in new List<RadioButton> { radioButton1, radioButton2, radioButton3 })
+            {
+                i.Checked = false;
+                if (i.Text == ans.Answers[0])
+                {
+                    i.Checked = true;
+                }
+            }
+
+            // 2
+
+            var ansSecondQuest = ans.Answers[1].Split(sep);
+
+            foreach (var box in new List<CheckBox> { checkBox1, checkBox2, checkBox3, checkBox4 })
+            {
+                box.Checked = false;
+                foreach (var str in ansSecondQuest)
+                {
+                    if (box.Text == str)
+                    {
+                        box.Checked = true;
+                    }
+                }
+            }
+
+            // 3
+            comboBox2.Text = ans.Answers[2];
+
+            // 4
+            if (Int32.TryParse(ans.Answers[3], out int answ1))
+                numericUpDown2.Value = answ1;
+
+            // 5
+            richTextBox1.Text = ans.Answers[4];
+
+        }
+
+
+        private void Update_ListBox()
+        {
+            listBox1.Items.Clear();
+
+            for (int i = 0; i < results.Count; ++i)
+            {
+                listBox1.Items.Add((i + 1).ToString() + ". " + results[i].ToString());
+            }
+        }
+
+        private void Clear_Form()
+        {
+            TestResult emptyAns = new TestResult("", "", "0", "", new List<string> { "", "", "", "0", "" });
+            comboBox1.SelectedIndex = -1;
+            comboBox2.SelectedIndex = -1;
+            Set_Ans(emptyAns);
+        }
+
+
+
+        // нажали сохранить(не в файл)
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TestResult ans = Get_Ans();
+
+            results.Add(ans);
+            Update_ListBox();
+        }
+
+        // автозаполнение
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Set_Ans(autoTestRes);
+        }
+
+        // сброс
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Очистить форму?(((((", "",
+                 MessageBoxButtons.YesNo,
+                 MessageBoxIcon.Exclamation);
+
+            if (dialogResult == DialogResult.Yes)
+            {
+                Clear_Form();
+            }
+        }
+
+        // изменился выбор в листбоксе
+        private void listBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            Set_Ans(results[listBox1.SelectedIndex]);
+        }
+
+        // нажали сохранить в файл
+        private void button4_Click(object sender, EventArgs e)
+        {
+            TestResult.Clear_File("out.json");
+            foreach (var i in results) i.SaveToFile("out.json");
+        }
+
+        // нажали загрузить файл
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var filePath = openFileDialog1.FileName;
+
+                foreach (var i in TestResult.ReadFile(filePath))
+                {
+                    results.Add(i);
+                }
+
+                Update_ListBox();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            results.Clear();
+            Update_ListBox();
+        }
+    }
+}
---
+++ kp2/kp2/TestResult.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Windows.Forms;
+
+
+namespace kp2
+{
+    class TestResult
+    {
+        string name, sername, age, group;
+        public string Name { get{return name;} }
+        public string Sername { get{return sername; } }
+        public string Age { get{return age; } }
+        public string Group { get{return group;} }
+
+         
+        List<string> answers;
+
+        public List<string> Answers { get { return answers; } }
+
+        public TestResult(string name, string sername, string age, string group, List<string> answers)
+        {
+            this.name = name;
+            this.sername = sername;
+            this.age = age;
+            this.group = group;
+            this.answers = answers;
+        }
+
+
+        public override string ToString()
+        {
+            var strAns = new List<string> { name, sername, age, group };
+            foreach (var i in answers) strAns.Add(i);
+
+            return string.Join(";", strAns);
+        }
+
+        private string Serialize()
+        {
+            string jsonStr = $"{{\n\"Name\": {name},\n" + $"\"Sername\": {sername},\n" +
+                $"\"Age\": {age},\n" + $"\"Group\": {group},\n" + $"\"Answers\": [\n";
+
+            foreach(var i in answers)
+            {
+                jsonStr += $"\"{i}\",\n";
+            }
+            jsonStr += "]\n}\n";
+
+            return jsonStr;
+        }
+
+        public void SaveToFile(string path)
+        {
+            var jsonStr = Serialize();
+
+            File.AppendAllText(path, jsonStr);
+        }
+
+
+
+        private static TestResult Deserialize(string jsonStr)
+        {
+            var helpList = new List<string>(jsonStr.Split(':'));
+
+
+            // приводим массив еще не распаршенных ответов на вопросы лучший вид
+            string strAns = helpList[helpList.Count() - 1];
+
+            strAns = strAns.Remove(strAns.Length - 1, 1).Remove(0, 1);
+            var helpAns = new List<string>(strAns.Split('\"'));
+            // находим и записываем ответы на вопросы
+            var realAns = new List<string>(5);
+            for (int i = 1; i < helpAns.Count; i += 2)
+            {
+                realAns.Add(helpAns[i]);
+            }
+
+
+            // удаляем ответы из массива для персональных данных
+            helpList.Remove(helpList[helpList.Count() - 1]);
+            helpList.Remove(helpList[0]);
+
+            // находим имя фамилию возраст и группу
+            var realPersonalData = new List<string>(5);
+            foreach (var i in helpList)
+            {
+                realPersonalData.Add(i.Split(',')[0].Remove(0, 1));
+            }
+
+
+            return new TestResult(realPersonalData[0], realPersonalData[1], realPersonalData[2], realPersonalData[3], realAns);
+        }
+
+        public static List<TestResult> ReadFile(string path)
+        {
+            var jsonStrs = new List<string>(File.ReadAllText(path).Split('}'));
+            jsonStrs.Remove(jsonStrs[jsonStrs.Count - 1]);
+
+            List<TestResult> resList = new List<TestResult>(30);
+
+            foreach (var s in jsonStrs)
+            {
+                resList.Add(Deserialize(s.Remove(0, 1)));
+            }
+
+            return resList;
+        }
+
+        public static void Clear_File(string path)
+        {
+            File.Delete(path);
+            File.Create(path).Close();
+        }
+
+
+
+
+
+    }
+}
